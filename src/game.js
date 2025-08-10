16522f38ea32ad3445dfa906eee356d5f0279581
Fiorella Queirolo <fiorellaqc17@gmail.com>
2025-08-10 20:32:35 +0200

    bubble changes when action clicked

--- src/game.js
+++ src/game.js
@@ -1,68 +1,80 @@
-const lifeBar100 = "art/life-bar/life_bar_100.png";
-const lifeBar75 = "art/life-bar/life_bar_75.png";
-const lifeBar50 = "art/life-bar/life_bar_50.png";
-const lifeBar25 = "art/life-bar/life_bar_25.png";
-const lifeBar0 = "art/life-bar/life_bar_0.png";
-const gameOver = "art/chinchilla/chinchilla_dead.png";
+const lifeBar = [
+  "art/life-bar/life_bar_0.png",
+  "art/life-bar/life_bar_25.png",
+  "art/life-bar/life_bar_50.png",
+  "art/life-bar/life_bar_75.png",
+  "art/life-bar/life_bar_100.png"
+];
+
+const bubbleActions = [
+  "art/bubble/bubble_eat.png",
+  "art/bubble/bubble_pet.png",
+  "art/bubble/bubble_sleep.png",
+  "art/bubble/bubble_wash.png"
+];
+
+const gameOverImg = "art/chinchilla/chinchilla_dead.png";
 let intervalId = null;
 let counterBar = 5;
 
-function getLifeBar(counterBar) {
-  switch (counterBar) {
-    case 5: return lifeBar100;
-    case 4: return lifeBar75;
-    case 3: return lifeBar50;
-    case 2: return lifeBar25; 
-    case 1: return lifeBar0;
+function getLifeBar() {
+  if (counterBar >= 1 || counterBar <= 5) {
+    return lifeBar[counterBar - 1];
   }
+  else {
+    console.log("Invalid counterBar value!");
+  }
+}
+
+function updateLifeBar() {
+  document.getElementById('lifeBar').src = getLifeBar(counterBar);
 }
 
 function increaseLifeBarByOne() {
-  if (counterBar == 5) {
+  if (counterBar < 5) {
+    counterBar++;
+    updateLifeBar();
+  }
+  else {
     counterBar = 5;
   }
-  else {
-    counterBar++;
-  }
-  console.log(`Counter: ${counterBar}`);
 }
 
-function isGameOver(counterBar) {
-  return counterBar <= 1;
+function decreaseLifeBarByOne() {
+  if (counterBar > 1) {
+    counterBar--;
+    updateLifeBar();
+  } 
+  else {
+    endGame();
+  }
 }
 
-intervalId = setInterval(() => {
-  if (isGameOver(counterBar)) {
-    document.getElementById('chinchillaArt').src = gameOver;
-    clearInterval(intervalId);
-    console.log('Game over!');
-  }
-  else {
-    document.getElementById('lifeBar').src = getLifeBar(counterBar);
-    counterBar--;   
-  }
-}, 2000);
+function endGame() {
+  clearInterval(intervalId);
+  document.getElementById('chinchillaArt').src = gameOverImg;
+  console.log("Game over!");
+}
+
+function selectRandomAction() {
+  let randomIndex = Math.floor(Math.random() * bubbleActions.length);
+  return bubbleActions[randomIndex];
+}
+
+function updateBubbleAction() {
+  document.getElementById('bubbleState').src = selectRandomAction();
+}
 
 document.addEventListener('DOMContentLoaded', function() {
-  const eat = document.getElementById('buttonEat');
-  const pet = document.getElementById('buttonPet');
-  const sleep = document.getElementById('buttonSleep'); 
-  const wash = document.getElementById('buttonWash'); 
-
-  eat.addEventListener('click', function() {
-    increaseLifeBarByOne();
-    document.getElementById('lifeBar').src = getLifeBar(counterBar);
-  }); 
-  pet.addEventListener('click', function() {
-    increaseLifeBarByOne();
-    document.getElementById('lifeBar').src = getLifeBar(counterBar);
-  });
-  sleep.addEventListener('click', function() {
-    increaseLifeBarByOne();
-    document.getElementById('lifeBar').src = getLifeBar(counterBar);
-  });
-  wash.addEventListener('click', function() {
-    increaseLifeBarByOne();
-    document.getElementById('lifeBar').src = getLifeBar(counterBar);
-  });
+  document.querySelectorAll('#buttonEat, #buttonPet, #buttonSleep, #buttonWash')
+    .forEach(btn => {
+      btn.addEventListener('click', () => {
+        increaseLifeBarByOne();
+        updateBubbleAction(); 
+      });
+    });
+  updateLifeBar();
+  intervalId = setInterval(() => {
+    decreaseLifeBarByOne();
+  }, 2000);
 });
