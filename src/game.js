4c12c3680aa87d14cfaa74ce1e23a28c28301705
Fiorella Queirolo <fiorellaqc17@gmail.com>
2025-08-10 18:52:39 +0200

    listeners actions buttons

--- src/game.js
+++ src/game.js
@@ -5,30 +5,64 @@ const lifeBar25 = "art/life-bar/life_bar_25.png";
 const lifeBar0 = "art/life-bar/life_bar_0.png";
 const gameOver = "art/chinchilla/chinchilla_dead.png";
 let intervalId = null;
-let counterBar = 1;
+let counterBar = 5;
 
 function getLifeBar(counterBar) {
   switch (counterBar) {
-    case 1: return lifeBar100;
-    case 2: return lifeBar75;
+    case 5: return lifeBar100;
+    case 4: return lifeBar75;
     case 3: return lifeBar50;
-    case 4: return lifeBar25; 
-    case 5: return lifeBar0;
+    case 2: return lifeBar25; 
+    case 1: return lifeBar0;
   }
 }
 
+function increaseLifeBarByOne() {
+  if (counterBar == 5) {
+    counterBar = 5;
+  }
+  else {
+    counterBar++;
+  }
+  console.log(`Counter: ${counterBar}`);
+}
+
 function isGameOver(counterBar) {
-  return counterBar >= 5;
+  return counterBar <= 1;
 }
 
 intervalId = setInterval(() => {
-  document.getElementById('lifeBar').src = getLifeBar(counterBar);
   if (isGameOver(counterBar)) {
     document.getElementById('chinchillaArt').src = gameOver;
     clearInterval(intervalId);
     console.log('Game over!');
   }
   else {
-    counterBar++;   
+    document.getElementById('lifeBar').src = getLifeBar(counterBar);
+    counterBar--;   
   }
 }, 2000);
+
+document.addEventListener('DOMContentLoaded', function() {
+  const eat = document.getElementById('buttonEat');
+  const pet = document.getElementById('buttonPet');
+  const sleep = document.getElementById('buttonSleep'); 
+  const wash = document.getElementById('buttonWash'); 
+
+  eat.addEventListener('click', function() {
+    increaseLifeBarByOne();
+    document.getElementById('lifeBar').src = getLifeBar(counterBar);
+  }); 
+  pet.addEventListener('click', function() {
+    increaseLifeBarByOne();
+    document.getElementById('lifeBar').src = getLifeBar(counterBar);
+  });
+  sleep.addEventListener('click', function() {
+    increaseLifeBarByOne();
+    document.getElementById('lifeBar').src = getLifeBar(counterBar);
+  });
+  wash.addEventListener('click', function() {
+    increaseLifeBarByOne();
+    document.getElementById('lifeBar').src = getLifeBar(counterBar);
+  });
+});
